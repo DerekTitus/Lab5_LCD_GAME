1e7ac062c382d463142c6d9ef081adec45ddc378
Colin Busho <C15Colin.Busho@C15CRBusho.usafa.ds.af.edu>
2013-11-03 11:49:52 -0700

    added my button and timer initializations as well as a global variable isGameOver so I know when to run my game, also added my timerA interrupt vector with an outline to do something when it hits two seconds

--- main.c
+++ main.c
@@ -6,18 +6,25 @@
 /*
  * main.c
  */
+
+char flag = 0;
+char isGameOver = 1;
+
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 
-    unsigned char player = initPlayer();
+    init_timer();
+    init_buttons();
+    __enable_interrupt();
 
-            init_timer();
-            init_buttons();
-            __enable_interrupt();
+    while(1)
+    {
+         	unsigned char player = initPlayer();
 
-            while(1)
-            {
-                    /*
+           	while (isGameOver)
+           	{
+
+            		/*
                      * while (game is on)
                      * {
                      *                 check if button is pushed (you don't want to block here, so don't poll!)
@@ -28,27 +35,56 @@ int main(void) {
                      *                         clear two second timer
                      *                         wait for button release (you can poll here)
                      * }
-                     *
-                     * print game over or you won, depending on game result
-                     *
-                     * wait for button press to begin new game (you can poll here)
-                     * wait for release before starting again
+
                      */
-            }
+           	}
+
+            	/*
+            	                     * print game over or you won, depending on game result
+            	                     *
+            	                     * wait for button press to begin new game (you can poll here)
+            	                     * wait for release before starting again
+            	                     */
+    }
 	
 	return 0;
 }
 
-//
-// YOUR TIMER A ISR GOES HERE
-//
+// Flag for continuous counting is TAIFG
+#pragma vector=TIMER0_A1_VECTOR
+__interrupt void TIMER0_A1_ISR()
+{
+    TACTL &= ~TAIFG;            // clear interrupt flag
+    flag += 1;
+    if(flag == 4)
+    {
+
+    }
+}
 
 void init_timer()
 {
-        // do timer initialization work
+    // do timer initialization work
+	TACTL &= ~(MC1|MC0);        // stop timer
+
+	TACTL |= TACLR;             // clear TAR
+
+	TACTL |= TASSEL1;           // configure for SMCLK - what's the frequency (roughly)?
+
+	TACTL |= ID1|ID0;           // divide clock by 8 - what's the frequency of interrupt?
+
+	TACTL &= ~TAIFG;            // clear interrupt flag
+
+	TACTL |= MC1;               // set count mode to continuous
+
+	TACTL |= TAIE;              // enable interrupt
+
+	__enable_interrupt();       // enable maskable interrupts
+
 }
 
 void init_buttons()
 {
-        // do button initialization work
+    // do button initialization work
+	configureP1PinAsButton(BIT1|BIT2|BIT3|BIT4);
 }
