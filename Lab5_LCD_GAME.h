026580e5b9104ee99e7008ffb70a152576b27f0d
Colin Busho <C15Colin.Busho@C15CRBusho.usafa.ds.af.edu>
2013-11-03 08:29:25 -0700

    first commit, this is the skeleton code to get the lab working it outlines ways to draw and move the player as well as spots to implement LCD and button initializations as well as button pushes

---
+++ Lab5_LCD_GAME.h
@@ -0,0 +1,45 @@
+/*
+ * Lab5_LCD_GAME.h
+ *
+ *  Created on: Nov 3, 2013
+ *      Author: C15Colin.Busho
+ */
+
+#ifndef LAB5_LCD_GAME_H_
+#define LAB5_LCD_GAME_H_
+
+#define UP 1
+#define DOWN 2
+#define LEFT 3
+#define RIGHT 4
+
+#define ROW_MASK 0x40
+
+#define NUM_MINES 2
+
+//
+// Initializes player to starting position on board.
+//
+unsigned char initPlayer();
+
+//
+// Prints the player marker (*) at the player location passed in.
+//
+void printPlayer(unsigned char player);
+
+//
+// Clears the location passed in.
+//
+void clearPlayer(unsigned char player);
+
+//
+// Given a player's current position and a direction, returns an updated player position.
+//
+unsigned char movePlayer(unsigned char player, unsigned char direction);
+
+//
+// Returns true if the position passed in is the winning game position.
+//
+char didPlayerWin(unsigned char player);
+
+#endif /* LAB5_LCD_GAME_H_ */
